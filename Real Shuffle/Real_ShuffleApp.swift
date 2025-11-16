153f2c35c5300c6bf6692c37aa4fbdef2f2b5267
Gonzalo Casal <gonzalocasal@gmail.com>
2025-11-16 08:04:25 -0300

    Initial Commit

---
+++ Real Shuffle/Real_ShuffleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Real_ShuffleApp.swift
+//  Real Shuffle
+//
+//  Created by Gonzalo Casal on 16/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct Real_ShuffleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
